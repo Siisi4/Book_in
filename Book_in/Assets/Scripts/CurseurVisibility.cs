3d011205fdc2296db82f76fbf9dea354f86a44c6
Simge Erdil <siisi4officiel@gmail.com>
2020-09-12 14:28:48 +0200

    curseur

---
+++ Book_in/Assets/Scripts/CurseurVisibility.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurseurVisibility : MonoBehaviour
+{
+    void Start()
+    {
+        //Set Cursor to be visible or not (true / false)
+        Cursor.visible = false;
+    }
+}
---
+++ Book_in/Assets/Scripts/Curseurtrue.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Curseurtrue : MonoBehaviour
+{
+
+    void Start()
+    {
+        //Set Cursor to be visible or not (true / false)
+        Cursor.visible = true;
+    }
+}
