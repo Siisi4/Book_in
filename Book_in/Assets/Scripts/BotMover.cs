82747271f591fcfd563535fdbbfcff3ba9040792
Simge Erdil <siisi4officiel@gmail.com>
2020-09-18 15:48:55 +0200

    navmesh agent basic + table asset

---
+++ Book_in/Assets/Scripts/BotMover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+
+public class BotMover : MonoBehaviour
+{
+    public Transform BotDirections;
+    
+
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = BotDirections.position;
+    }
+
+}
