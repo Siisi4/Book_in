5e654b3335afe5e930be2e3af894a647d93d7692
Simge Erdil <siisi4officiel@gmail.com>
2020-10-07 17:54:07 +0200

    paintme()

---
+++ Book_in/Assets/Scripts/Paint.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paint : MonoBehaviour
+{
+    public GameObject projectile;
+    public Transform spawnPoint;
+
+    private void Paintme()
+    {
+        Vector3 position = spawnPoint.position;
+        Quaternion rotation = spawnPoint.rotation;
+
+        Instantiate(projectile, position, rotation);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+            Paintme();
+    }
+}
