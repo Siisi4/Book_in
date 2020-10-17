56cb5a94e51190c805e582c22567fc0afc3dc2e9
Simge Erdil <siisi4officiel@gmail.com>
2020-10-17 03:01:04 +0200

    porte entree & sortie

---
+++ Book_in/Assets/Scripts/Entree.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entree : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public GameObject Player;
+    public Transform tp;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            CharacterController cc = Player.GetComponent<CharacterController>();
+            cc.enabled = false;
+            Player.transform.position = tp.position;
+            cc.enabled = true;
+        }   
+    }
+}
