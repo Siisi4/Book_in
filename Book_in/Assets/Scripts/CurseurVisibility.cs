825011875391be71c2f672e0a2a9e6d650e128d9
Simge Erdil <siisi4officiel@gmail.com>
2020-10-19 00:03:12 +0200

    env. + skybox

--- Book_in/Assets/Scripts/CurseurVisibility.cs
+++ Book_in/Assets/Scripts/CurseurVisibility.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class CurseurVisibility : MonoBehaviour
 {
+    private void Update()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+    }
     void Start()
     {
         //Set Cursor to be visible or not (true / false)
