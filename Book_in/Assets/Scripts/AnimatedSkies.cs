53a9d5f4645007e74f5585e3ec46770929ec06d7
Simge Erdil <siisi4officiel@gmail.com>
2020-10-19 01:56:30 +0200

    sky jour - nuit anim Time

--- Book_in/Assets/Scripts/AnimatedSkies.cs
+++ Book_in/Assets/Scripts/AnimatedSkies.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class AnimatedSkies : MonoBehaviour
 {
+    float scrollSpeed = 0.001f;
+    Renderer rend;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rend = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float offset = Time.time * scrollSpeed;
+        rend.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
     }
 }
