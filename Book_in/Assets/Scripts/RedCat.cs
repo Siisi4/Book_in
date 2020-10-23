143a4e1112d3bfd015db407c05e25a4d5977baf7
Siisi4 <siisi4officiel@gmail.com>
2020-10-23 14:40:41 +0200

    RedCatScript

---
+++ Book_in/Assets/Scripts/RedCat.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedCat : MonoBehaviour
+{
+
+    public GameObject CanvasM;
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            CanvasM.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            CanvasM.SetActive(false);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
