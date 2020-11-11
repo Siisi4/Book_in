3af2d14f1a6303c0f46e0b1fdfa4aa0a1b5d8310
Simge Erdil <siisi4officiel@gmail.com>
2020-11-11 03:55:31 +0100

    save corrrection + scrollbar size

---
+++ Book_in/Assets/Scripts/Saves Scripts/TextManager.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using TMPro;
+
+
+[System.Serializable]
+public class Data
+{
+
+    public Dictionary<string, string> livretexte = new Dictionary<string, string>();
+
+    void Clear()
+    {
+        livretexte.Clear();
+    }
+
+}
+public class TextManager 
+{
+    private static string path = Path.Combine(Application.dataPath, "data.dat");
+    private static Data data = new Data();
+    private static bool loaded = false;
+
+
+
+    public static void Load()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file;
+
+        try
+        {
+            file = File.Open(path, FileMode.Open);
+        }
+        catch (FileNotFoundException)
+        {
+            return;
+        }
+        try
+        {
+            data = bf.Deserialize(file) as Data;
+            loaded = true;
+        }
+        finally
+        {
+            file.Close();
+        }
+
+    }
+
+
+    public static void Save()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file;
+
+        file = File.Open(path, FileMode.Create);
+
+        try
+        {
+            bf.Serialize(file, data);
+        }
+        finally
+        {
+            file.Close();
+        }
+    }
+
+    // Appel des sauvegardes
+
+    public static void SetBook(string name, TMP_Text texte)
+    {
+        data.livretexte[name] = texte.text;
+        Save();
+        Debug.Log("Sauvegardé");
+    }
+
+    public static string GetBook (string name)
+    {
+        if (!loaded)
+        {
+            Load();
+        }
+
+        string livre = string.Empty;
+        if (data.livretexte.ContainsKey(name))
+        {
+            livre = data.livretexte[name];
+        }
+        return livre;
+    }
+}
--- Book_in/Assets/Scripts/Saves Scripts/Writing1Load.cs
+++ Book_in/Assets/Scripts/Saves Scripts/Writing1Load.cs
@@ -10,25 +10,20 @@ public class Writing1Load : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Livreecrit.text = PlayerPrefs.GetString("Slot1", "");
+        //Livreecrit.text = PlayerPrefs.GetString("Slot1", "");
 
         // Je load le texte enregistré sous la clé "essay"
 
-        //Livreecrit.text = TextManager.GetBook("essay");
+        Livreecrit.text = TextManager.GetBook("essay");
 
     }
 
     // Fonction appelée quand cliquer sur "sauvegarder"
-    //public void SauvegardeLivre()
-    //{
-        // Vas sauvegarder à la clé "essay", le TMP_Text Livreecrit
-        //TextManager.SetBook("essay", Livreecrit);
-    //}
-
-
-    // Update is called once per frame
-    void Update()
+    public void SauvegardeLivre()
     {
-        
+        // Vas sauvegarder à la clé "essay", le TMP_Text Livreecrit
+        TextManager.SetBook("essay", Livreecrit);
     }
+
+
 }
