0d92959ebf3e7e7ee76afe058b2da1c49b9af920
Simge Erdil <siisi4officiel@gmail.com>
2020-11-19 15:44:48 +0100

    missions canvas manager

---
+++ Book_in/Assets/Scripts/Missions/ClickonLogo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickonLogo : MonoBehaviour
+{
+
+    public AudioSource pencilsound;
+    public Animation anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayPencilSound(){
+        pencilsound.Play() ;
+    }
+
+    public void PlayGrowingAnimation(){
+        anim.Play();
+    }
+
+
+
+}
---
+++ Book_in/Assets/Scripts/Missions/MissionsCanvasOpener.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionsCanvasOpener : MonoBehaviour
+{
+    public GameObject ChatCanvas;
+    public GameObject PapiCanvas;
+    public GameObject RenneCanvas;
+    public GameObject RenardCanvas;
+    public GameObject OursCanvas;
+
+    public void openchatcanvas(){
+        ChatCanvas.SetActive(true);
+        PapiCanvas.SetActive(false);
+        RenneCanvas.SetActive(false);
+        RenardCanvas.SetActive(false);
+        OursCanvas.SetActive(false);
+    }
+
+    public void openpapicanvas(){
+        PapiCanvas.SetActive(true);
+        ChatCanvas.SetActive(false);
+        RenneCanvas.SetActive(false);
+        RenardCanvas.SetActive(false);
+        OursCanvas.SetActive(false);
+    }
+
+    public void openrennecanvas(){
+        RenneCanvas.SetActive(true);
+        ChatCanvas.SetActive(false);
+        PapiCanvas.SetActive(false);
+        RenardCanvas.SetActive(false);
+        OursCanvas.SetActive(false);
+    }
+
+    public void openrenardcanvas(){
+        RenardCanvas.SetActive(true);
+        ChatCanvas.SetActive(false);
+        PapiCanvas.SetActive(false);
+        RenneCanvas.SetActive(false);
+        OursCanvas.SetActive(false);
+    }
+
+    public void openourscanvas(){
+        OursCanvas.SetActive(true);
+        ChatCanvas.SetActive(false);
+        PapiCanvas.SetActive(false);
+        RenneCanvas.SetActive(false);
+        RenardCanvas.SetActive(false);
+    }
+}
