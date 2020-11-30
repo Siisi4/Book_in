7010f107cc7619840b741e74534da7b9ff292546
Simge Erdil <siisi4officiel@gmail.com>
2020-11-30 21:24:03 +0100

    drag script + audioplayer

---
+++ Book_in/Assets/Scripts/Sons/DragUI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragUI : MonoBehaviour, IDragHandler
+{
+    [SerializeField] private RectTransform dragRectTransform;
+    [SerializeField] private Canvas canvas;
+
+    public void OnDrag(PointerEventData eventData){
+        dragRectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+ 
+}
---
+++ Book_in/Assets/Scripts/Sons/VolumeSlider.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class VolumeSlider : MonoBehaviour
+{
+    // chercher l'audio source
+    private AudioSource source;
+
+    // Valeur du slider
+    private float musicVolume = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // définir la source audio 
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // option de volume de source = valeur du slider
+        source.volume = musicVolume;
+    }
+
+    // fonction à ajouter au slider avec un float en paramètre
+    public void VolumeMusique(float vol){
+        musicVolume = vol;
+    }
+
+}
