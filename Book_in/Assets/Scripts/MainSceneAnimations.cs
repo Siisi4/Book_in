fd089b914767aa20e20aad4795d76bdc3d581001
Simge Erdil <siisi4officiel@gmail.com>
2020-12-08 12:08:00 +0100

    shroom light anim

---
+++ Book_in/Assets/Scripts/MainSceneAnimations.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainSceneAnimations : MonoBehaviour
+{
+    Animation animLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animLight = gameObject.GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animLight.Play();
+    }
+}
