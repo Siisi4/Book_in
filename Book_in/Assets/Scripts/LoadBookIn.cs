32ce95771879d0b508e4f9aebd0a5c68945d3045
Simge Erdil <siisi4officiel@gmail.com>
2020-12-08 15:04:05 +0100

    book loading

---
+++ Book_in/Assets/Scripts/LoadBookIn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LoadBookIn : MonoBehaviour
+{
+    public TMP_Text BookInLoadText;
+    public GameObject LoadingScreen;
+    float textsize;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textsize = 76;
+        if(BookInLoadText.fontSize == textsize ){
+            LoadingScreen.SetActive(false);
+        }
+    }
+}
