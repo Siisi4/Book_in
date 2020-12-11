72f7c6c1a2b629f9c91a09fbdf56edda836ae6db
Simge Erdil <siisi4officiel@gmail.com>
2020-12-11 19:36:16 +0100

    tableau C

--- Book_in/Assets/Scripts/Missions/OpenTableax.cs
+++ Book_in/Assets/Scripts/Missions/OpenTableax.cs
@@ -8,16 +8,20 @@ public class OpenTableax : MonoBehaviour
     
     public GameObject TableauA;
     public GameObject TableauB;
+    public GameObject TableauC;
     public GameObject BoutonMasquer;
     public GameObject BoutonMasquerB;
+    public GameObject BoutonMasquerC;
 
     
     // Tableau A
     public void OpenTableauA(){
         TableauA.SetActive(true);
         TableauB.SetActive(false);
+        TableauC.SetActive(false);
         BoutonMasquer.SetActive(true);
         BoutonMasquerB.SetActive(false);
+        BoutonMasquerC.SetActive(false);
     }
     public void CloseTableauA(){
         TableauA.SetActive(false);
@@ -27,12 +31,27 @@ public class OpenTableax : MonoBehaviour
    public void OpenTableauB(){
        TableauB.SetActive(true);
        TableauA.SetActive(false);
+       TableauC.SetActive(false);
        BoutonMasquer.SetActive(false);
+       BoutonMasquerC.SetActive(false);
        BoutonMasquerB.SetActive(true);
    }
 
    public void CloseTableauB(){
        TableauB.SetActive(false);
        BoutonMasquerB.SetActive(false);
+   }
+
+   public void OpenTableauC(){
+       TableauC.SetActive(true);
+       TableauB.SetActive(false);
+       TableauA.SetActive(false);
+       BoutonMasquer.SetActive(false);
+       BoutonMasquerB.SetActive(false);
+       BoutonMasquerC.SetActive(true);
+   }
+   public void CloseTableauC(){
+       TableauC.SetActive(false);
+       BoutonMasquerC.SetActive(false);
    }
 }
