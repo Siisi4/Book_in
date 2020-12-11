d7b0457974da9db9115e1b08df033162f699a7dd
Simge Erdil <siisi4officiel@gmail.com>
2020-12-11 18:45:19 +0100

    tableau B

---
+++ Book_in/Assets/Scripts/Missions/DragTableaux.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragTableaux : MonoBehaviour, IDragHandler
+{
+    [SerializeField] private RectTransform dragRectTransform;
+    [SerializeField] private Canvas canvas;
+    public void OnDrag(PointerEventData eventData){
+        dragRectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+}
---
+++ Book_in/Assets/Scripts/Missions/OpenTableax.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpenTableax : MonoBehaviour
+{
+    
+    public GameObject TableauA;
+    public GameObject TableauB;
+    public GameObject BoutonMasquer;
+    public GameObject BoutonMasquerB;
+
+    
+    // Tableau A
+    public void OpenTableauA(){
+        TableauA.SetActive(true);
+        TableauB.SetActive(false);
+        BoutonMasquer.SetActive(true);
+        BoutonMasquerB.SetActive(false);
+    }
+    public void CloseTableauA(){
+        TableauA.SetActive(false);
+        BoutonMasquer.SetActive(false);
+    }
+   // Tableau B 
+   public void OpenTableauB(){
+       TableauB.SetActive(true);
+       TableauA.SetActive(false);
+       BoutonMasquer.SetActive(false);
+       BoutonMasquerB.SetActive(true);
+   }
+
+   public void CloseTableauB(){
+       TableauB.SetActive(false);
+       BoutonMasquerB.SetActive(false);
+   }
+}
